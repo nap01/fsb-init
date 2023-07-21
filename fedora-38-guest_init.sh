b4b2d036ec39b56e10c46615de9bf62c558c64b2
nap01 <naveja.personal@gmail.com>
2023-07-21 13:11:09 -0700

    Updated ubuntu guest script

--- fedora-38-guest_init.sh
+++ fedora-38-guest_init.sh
@@ -3,10 +3,6 @@
 # Import Enviroment Variables
 set -a; source .env; set +a
 
-# Git Config
-git config --global user.name "$FULLNAME"
-git config --global user.email $EMAIL
-
 # Install VSCode
 #sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 #sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
